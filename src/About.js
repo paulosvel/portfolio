844486dee43c240aa0896582380843012b47c11b
paulosvel <paul2001vel@gmail.com>
2023-03-31 20:18:12 +0300

    changes

---
+++ src/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+const About = () => {
+  return (
+    <p
+      className="about"
+      id="about"
+      style={{ width: "30%", textAlign: "center", height: "100vh" }}
+    >
+      Hello! My name is Paulos, and I am a self-taught full stack web developer
+      who is passionate about creating beautiful and functional websites and
+      applications. I am currently studying software development and constantly
+      expanding my knowledge and skills to stay up-to-date with the latest
+      technologies and trends in the industry. I believe that good web design
+      should be both aesthetically pleasing and easy to use, and I strive to
+      create user-friendly and responsive websites that meet the needs of my
+      clients and their customers.In my free time, I enjoy experimenting with
+      new coding techniques and exploring the possibilities of web development.
+      I am always on the lookout for exciting new projects and collaborations,
+      so please don't hesitate to get in touch if you're interested in working
+      together!"
+    </p>
+  );
+};
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,109 +1,129 @@
+import { HashLink } from "react-router-hash-link";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-
+import About from "./About";
+import React from "react";
 function App() {
   return (
-    <>
-      <div className="navbar" style={{ color: "#CCD6F6", fontFamily:'"SF Mono","Fira Code","Fira Mono","Roboto Mono",monospace' }}>
-        <a
-          href="/#about"
-          className="about"
+    <BrowserRouter>
+      <>
+        <div
+          className="navbar"
           style={{
-            paddingRight: "2%",
-            fontFamily:'"SF Mono","Fira Code","Fira Mono","Roboto Mono",monospace',
-            cursor: "pointer",
-          }}
-        >
-          <span style={{ color: "#64ffda" }}>01.</span> About{" "}
-        </a>
-        <a
-          className="experience"
-          style={{
-            paddingRight: "2%",
-            fontFamily:'"SF Mono","Fira Code","Fira Mono","Roboto Mono",monospace',
-            cursor: "pointer",
-          }}
-        >
-          <span style={{ color: "#64ffda" }}>02.</span>Experience{" "}
-        </a>
-        <a
-          className="projects"
-          style={{
-            paddingRight: "2%",
-            fontFamily:'"SF Mono","Fira Code","Fira Mono","Roboto Mono",monospace',
-            cursor: "pointer",
-          }}
-        >
-          <span style={{ color: "#64ffda" }}>03.</span>Projects{" "}
-        </a>
-        <a
-          className="contact"
-          style={{
-            paddingRight: "2%",
-            fontFamily:'"SF Mono","Fira Code","Fira Mono","Roboto Mono",monospace',
-            cursor: "pointer",
-          }}
-        >
-          <span style={{ color: "#64ffda" }}>04.</span>Contact{" "}
-        </a>
-        <button
-          className="resume"
-          style={{
-            color: "#64ffda",
-            fontFamily: "SF-Mono",
-            backgroundColor: "transparent",
-            borderRadius: "4px",
-            border: "1px solid #64ffda",
-            cursor: "pointer",
-            padding:'0.75rem 1rem',
-            fontFamily:'"SF Mono","Fira Code","Fira Mono","Roboto Mono",monospace',
-            
-          }}
-        >
-          Resume
-        </button>
-      </div>
-      <div className="App">
-        <h1
-          style={{
-            fontSize: "16px",
+            color: "#CCD6F6",
             fontFamily:
               '"SF Mono","Fira Code","Fira Mono","Roboto Mono",monospace',
-            margin: "0",
+            position: "sticky",
+            top: "0",
           }}
         >
-          Hi, my name is
-        </h1>
-        <p style={{ fontSize: "80px", margin: "0", fontWeight: "bold" }}>
-          Paulos Velissarakos.
-        </p>
-        <p
-          style={{
-            color: "#8892B0",
-            fontSize: "80px",
-            margin: "0",
-            fontFamily:
-              '"Calibre","Inter","San Francisco","SF Pro Text",-apple-system,system-ui,sans-serif"',
-            fontWeight: "bold",
-          }}
-        >
-          I build things for the web.
-        </p>
-        <p
-          style={{
-            color: "#8892B0",
-            fontFamily:
-              '"Calibre","Inter","San Francisco","SF Pro Text",-apple-system,system-ui,sans-serif',
-            width: "40%",
-            fontSize: "20",
-          }}
-        >
-          I am a self-taught full stack developer with a passion for building
-          web applications. Through self-directed learning and practical
-          application, I have gained a deep understanding of both front-end and
-          back-end development.
-        </p>
-      </div>
-    </>
+          <HashLink
+            to = "#about" 
+            className="about"
+            style={{
+              paddingRight: "2%",
+              fontFamily:
+                '"SF Mono","Fira Code","Fira Mono","Roboto Mono",monospace',
+              cursor: "pointer",
+              textDecoration: "none",
+            }}
+          smooth>
+            <span style={{ color: "#64ffda" }}>01.</span> About{" "}
+          </HashLink>
+          <a
+            className="experience"
+            style={{
+              paddingRight: "2%",
+              fontFamily:
+                '"SF Mono","Fira Code","Fira Mono","Roboto Mono",monospace',
+              cursor: "pointer",
+            }}
+          >
+            <span style={{ color: "#64ffda" }}>02.</span>Experience{" "}
+          </a>
+          <a
+            className="projects"
+            style={{
+              paddingRight: "2%",
+              fontFamily:
+                '"SF Mono","Fira Code","Fira Mono","Roboto Mono",monospace',
+              cursor: "pointer",
+            }}
+          >
+            <span style={{ color: "#64ffda" }}>03.</span>Projects{" "}
+          </a>
+          <a
+            className="contact"
+            style={{
+              paddingRight: "2%",
+              fontFamily:
+                '"SF Mono","Fira Code","Fira Mono","Roboto Mono",monospace',
+              cursor: "pointer",
+            }}
+          >
+            <span style={{ color: "#64ffda" }}>04.</span>Contact{" "}
+          </a>
+          <button
+            className="resume"
+            style={{
+              color: "#64ffda",
+              fontFamily: "SF-Mono",
+              backgroundColor: "transparent",
+              borderRadius: "4px",
+              border: "1px solid #64ffda",
+              cursor: "pointer",
+              padding: "0.75rem 1rem",
+              fontFamily:
+                '"SF Mono","Fira Code","Fira Mono","Roboto Mono",monospace',
+            }}
+          >
+            Resume
+          </button>
+        </div>
+        <div className="App">
+          <h1
+            style={{
+              fontSize: "16px",
+              fontFamily:
+                '"SF Mono","Fira Code","Fira Mono","Roboto Mono",monospace',
+              margin: "0",
+            }}
+          >
+            Hi, my name is
+          </h1>
+          <p style={{ fontSize: "80px", margin: "0", fontWeight: "bold" }}>
+            Paulos Velissarakos.
+          </p>
+          <p
+            style={{
+              color: "#8892B0",
+              fontSize: "80px",
+              margin: "0",
+              fontFamily:
+                '"Calibre","Inter","San Francisco","SF Pro Text",-apple-system,system-ui,sans-serif"',
+              fontWeight: "bold",
+            }}
+          >
+            I build things for the web.
+          </p>
+          <p
+            style={{
+              color: "#8892B0",
+              fontFamily:
+                '"Calibre","Inter","San Francisco","SF Pro Text",-apple-system,system-ui,sans-serif',
+              width: "40%",
+              fontSize: "20",
+            }}
+          >
+            I am a self-taught full stack developer with a passion for building
+            web applications. Through self-directed learning and practical
+            application, I have gained a deep understanding of both front-end
+            and back-end development.
+          </p>
+        </div>
+      </>
+      <About />
+    </BrowserRouter>
   );
 }
 
